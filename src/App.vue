8cb1b8cf317f84cf9ed4ee5d77a3e1e879c2113a
Sergi Mateo <sergimateo@gmail.com>
2021-03-01 17:58:43 +0100

    avanzamos...

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <template>
   <div id="app">
-    pan
+    
+      <ModalBootstrap>
+        <template v-slot:modalHeader>
+         <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+           
       
+      
+      </template>
+      <template v-slot:modalBody>
+       <div class="modal-body">
+        ...
+      </div>
+      </template>
+      <template v-slot:modalFooter>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+       </template>
+
+      </ModalBootstrap>
     
   </div>
 </template>
 
 <script type="text/javascript" src="../src/Scripts/app.js"></script>
-<style src="../src/Styles/CSS/app.css"></style>
+<style src="../src/Styles/CSS/app.css" scoped></style>
---
+++ src/components/ModalBootstrap.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+  
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop">
+  Launch static backdrop modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <slot name="modalHeader"></slot>
+      <slot name="modalBody"></slot>
+      <slot name="modalFooter"></slot>
+    </div>
+  </div>
+</div>
+  </div>
+</template>
+
+<script type="text/javascript" src="@/Scripts/modalBootstrap.js"></script>
+<style src="@/Styles/CSS/modal-bootstrap.css"></style>
